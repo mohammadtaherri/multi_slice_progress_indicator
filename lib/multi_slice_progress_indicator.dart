73dbd66b5e9f45ddef543874481d6d75cdc8a369
mohammadtaherri <taherimohammad08@gmail.com>
2022-11-11 15:01:06 +0330

    refactor: rename controller.dart to animation.dart

--- lib/multi_slice_progress_indicator.dart
+++ lib/multi_slice_progress_indicator.dart
@@ -7,6 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 part './src/constants.dart';
-part './src/controller.dart';
+part 'src/animation.dart';
 part 'src/rendering.dart';
 part './src/widget.dart';
